7ba224e7bb5f2dfe5d81c0091f29249f4a3ec897
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-10-05 06:46:49 -0700

    day5 - database - install mysql driver

--- 04-database/01-intro/go.mod
+++ 04-database/01-intro/go.mod
@@ -2,4 +2,7 @@ module database/06/01
 
 go 1.21.1
 
-require github.com/google/uuid v1.3.1
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/google/uuid v1.3.1
+)
--- 04-database/01-intro/main.go
+++ 04-database/01-intro/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+	// the '_' is used to import the package without using it
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
@@ -17,5 +22,10 @@ func NewProduct(id, name string, price float64) *Product {
 }
 
 func main() {
+	db, err := sql.Open("mysql", "docker:docker@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
 }
