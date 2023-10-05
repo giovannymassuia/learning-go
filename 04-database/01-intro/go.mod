f9002cb5a03cbeb62d8c972de30a3739fb08ecd7
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-10-05 06:41:20 -0700

    day5 - database - initial configs

---
+++ 04-database/01-intro/go.mod
@@ -0,0 +1,5 @@
+module database/06/01
+
+go 1.21.1
+
+require github.com/google/uuid v1.3.1
---
+++ 04-database/schema.sql
@@ -0,0 +1,7 @@
+create table products
+(
+    id  varchar(255),
+    name  varchar(255),
+    price decimal(10, 2),
+    primary key (id)
+);
