c03ea7ed1734fafb5835dd6d95dd65728b8b7945
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-10-13 12:57:59 -0700

    day8 - api - configs

--- 07-api/cmd/server/main.go
+++ 07-api/cmd/server/main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "github.com/giovannymassuia/learning-go/07-api/configs"
 
+func main() {
+	config, _ := configs.LoadConfig(".")
+	println(config.DBHost)
 }
---
+++ 07-api/configs/config.go
@@ -0,0 +1,45 @@
+package configs
+
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
+var cfg *conf
+
+type conf struct {
+	DBDriver string `mapstructure:"DB_DRIVER"`
+	DBHost   string `mapstructure:"DB_HOST"`
+	DBPort   string `mapstructure:"DB_PORT"`
+	DBUser   string `mapstructure:"DB_USER"`
+	DBPass   string `mapstructure:"DB_PASS"`
+	DBName   string `mapstructure:"DB_NAME"`
+
+	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+
+	JWTSecret    string `mapstructure:"JWT_SECRET"`
+	JWTExpiresIn int    `mapstructure:"JWT_EXPIRES_IN"`
+	TokenAuth    *jwtauth.JWTAuth
+}
+
+func LoadConfig(path string) (*conf, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+
+	return cfg, nil
+}
