f1176a10944a4124ff2a06e39928c342e4d95bee
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-10-13 13:24:18 -0700

    day8 - api - entity product

---
+++ 07-api/internal/entity/product.go
@@ -0,0 +1,54 @@
+package entity
+
+import (
+	"errors"
+	"github.com/giovannymassuia/learning-go/07-api/pkg/entity"
+	"time"
+)
+
+var (
+	ErrIDIsRequired    = errors.New("id is required")
+	ErrInvalidID       = errors.New("invalid id")
+	ErrNameIsRequired  = errors.New("name is required")
+	ErrPriceIsRequired = errors.New("price is required")
+	ErrInvalidPrice    = errors.New("invalid price")
+)
+
+type Product struct {
+	ID        entity.ID `json:"id"`
+	Name      string    `json:"name"`
+	Price     int       `json:"price"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func NewProduct(name string, price int) (*Product, error) {
+	product := &Product{
+		ID:        entity.NewID(),
+		Name:      name,
+		Price:     price,
+		CreatedAt: time.Now(),
+	}
+	if err := product.Validate(); err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (p *Product) Validate() error {
+	if p.ID.String() == "" {
+		return ErrIDIsRequired
+	}
+	if _, err := entity.ParseID(p.ID.String()); err != nil {
+		return ErrInvalidID
+	}
+	if p.Name == "" {
+		return ErrNameIsRequired
+	}
+	if p.Price == 0 {
+		return ErrPriceIsRequired
+	}
+	if p.Price < 0 {
+		return ErrInvalidPrice
+	}
+	return nil
+}
---
+++ 07-api/internal/entity/product_test.go
@@ -0,0 +1,41 @@
+package entity
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewProduct(t *testing.T) {
+	p, err := NewProduct("Product 1", 100)
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.NotEmpty(t, p.ID)
+	assert.Equal(t, "Product 1", p.Name)
+	assert.Equal(t, 100, p.Price)
+}
+
+func TestProductWhenNameIsRequired(t *testing.T) {
+	p, err := NewProduct("", 100)
+	assert.NotNil(t, err)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrNameIsRequired, err)
+}
+
+func TestProductWhenPriceIsRequired(t *testing.T) {
+	p, err := NewProduct("Product 1", 0)
+	assert.NotNil(t, err)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrPriceIsRequired, err)
+}
+
+func TestProductWhenPriceIsInvalid(t *testing.T) {
+	p, err := NewProduct("Product 1", -1)
+	assert.NotNil(t, err)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrInvalidPrice, err)
+}
+
+func TestProduct_Validate(t *testing.T) {
+	p, _ := NewProduct("Product 1", 100)
+	assert.Nil(t, p.Validate())
+}
