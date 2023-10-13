903f126745ba72c52931e5cf8daac9358bb4b653
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-10-12 19:49:17 -0700

    day7 - testing - batch tests

--- 06-testing/01-intro/tax_test.go
+++ 06-testing/01-intro/tax_test.go
@@ -12,3 +12,22 @@ func TestCalculateTax(t *testing.T) {
 		t.Errorf("CalculateTax(%f): expected %f, got %f", amount, expected, result)
 	}
 }
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expected float64
+	}
+
+	tests := []calcTax{
+		{amount: 500, expected: 5.0},
+		{amount: 1000, expected: 10.0},
+		{amount: 1500, expected: 10.0},
+	}
+
+	for _, tt := range tests {
+		result := CalculateTax(tt.amount)
+		if result != tt.expected {
+			t.Errorf("CalculateTax(%f): expected %f, got %f", tt.amount, tt.expected, result)
+		}
+	}
+}
