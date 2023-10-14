c2804effc63953a8f1a5c84880bffaf71ac2271a
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-10-14 14:08:44 -0700

    day9 - multi-threading - concurrency issue - atomic ops

--- 08-multi-threading/03-concurrency-issue/main.go
+++ 08-multi-threading/03-concurrency-issue/main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
+	"sync/atomic"
 	"time"
 )
 
 var number uint64 = 0
 
+// `go run -race main.go`: Used to debug or find race conditions in the code.
+
 func main() {
-	m := sync.Mutex{}
+	//m := sync.Mutex{}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		m.Lock()
-		number++
-		m.Unlock()
+		//m.Lock()
+		//number++
+		atomic.AddUint64(&number, 1) // Atomic operations are thread safe
+		//m.Unlock()
 		time.Sleep(300 * time.Millisecond)
 		w.Write([]byte(fmt.Sprintf("Number: %d\n", number)))
 	})
