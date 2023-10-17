c19473fceea12afef86511a6db7d58b12e532154
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-10-16 21:58:19 -0700

    day11 - upload s3 - serial approach

---
+++ 13-upload-s3/cmd/uploader/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"io"
+	"os"
+)
+
+var (
+	s3Client *s3.S3
+	s3Bucket string
+)
+
+// init() is called before main()
+func init() {
+
+	// config using for aws sso credentials
+	sess, err := session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+		Profile:           os.Getenv("AWS_PROFILE"),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	s3Client = s3.New(sess)
+	s3Bucket = "learning-go-upload-s3-example"
+}
+
+func main() {
+	dir, err := os.Open("./tmp")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+
+	for {
+		files, err := dir.Readdir(1)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("Error reading directory: %s\n", err.Error())
+			continue
+		}
+
+		uploadFile(files[0].Name())
+	}
+}
+
+func uploadFile(filename string) {
+	completeFileName := fmt.Sprintf("./tmp/%s", filename)
+	f, err := os.Open(completeFileName)
+	if err != nil {
+		fmt.Printf("Error opening file %s: %s\n", completeFileName, err.Error())
+		return
+	}
+	defer f.Close()
+
+	// upload file to S3
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: &s3Bucket,
+		Key:    aws.String(filename),
+		Body:   f,
+	})
+	if err != nil {
+		fmt.Printf("Error uploading file %s: %s\n", completeFileName, err.Error())
+		return
+	}
+
+	fmt.Printf("File %s uploaded successfully\n", completeFileName)
+}
---
+++ 13-upload-s3/go.mod
@@ -0,0 +1,7 @@
+module 13-upload-s3
+
+go 1.21
+
+require github.com/aws/aws-sdk-go v1.45.26
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
