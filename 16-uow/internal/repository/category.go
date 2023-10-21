09d6570d47a6099e9431cab2a097c2a5831d7e5b
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-10-21 16:39:19 -0700

    day13 - uow

---
+++ 16-uow/internal/repository/category.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"16-uow/internal/db"
+	"16-uow/internal/entity"
+)
+
+type CategoryRepositoryInterface interface {
+	Insert(ctx context.Context, category entity.Category) error
+}
+
+type CategoryRepository struct {
+	DB      *sql.DB
+	Queries *db.Queries
+}
+
+func NewCategoryRepository(dtb *sql.DB) *CategoryRepository {
+	return &CategoryRepository{
+		DB:      dtb,
+		Queries: db.New(dtb),
+	}
+}
+
+func (r *CategoryRepository) Insert(ctx context.Context, category entity.Category) error {
+	return r.Queries.CreateCategory(ctx, db.CreateCategoryParams{
+		Name: category.Name,
+	})
+}
